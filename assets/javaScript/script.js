cd30b576c0df80d0fcf2ffda59aca7451389bd61
asmaaAlmohammed-webDev <asmaa811almohammed@gmail.com>
2024-10-19 21:11:20 +0300

    first commit

---
+++ assets/javaScript/script.js
@@ -0,0 +1,20 @@
+const input = document.getElementById("inputBox");
+const buttons = document.querySelectorAll("button");
+let string = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      input.value = eval(string);
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
